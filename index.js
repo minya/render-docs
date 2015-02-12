ba48faddae7a53d69980ae3a6900434af1c4d1a4
Mikhail Brel <m@brel.me>
2015-02-12 19:34:33 +0500

    cosmetics

--- index.js
+++ index.js
@@ -4,59 +4,64 @@ var fs = require('fs');
 var through = require('through2');
 var toc = require('marked-toc');
 var pdf = require('html-pdf');
-var path = require('path')
+var path = require('path');
 
 module.exports = {
-	 renderPdf: function() {
-		return through.obj(function(file, enc, cb) {
-			var html = file.contents.toString(enc);
-			var options = {
-				format: 'A4',
-				border: '1cm',
-				type: 'pdf'
-			};
-			pdf.create(html, options, function(err, buffer) {
-				file.path = file.base + file.relative.replace('.html', '.pdf');
-				file.contents = buffer;
-				cb(null, file);
-			});
-		});
-	},
+     renderPdf: function() {
+        return through.obj(function(file, enc, cb) {
+            var html = file.contents.toString(enc);
+            var options = {
+                format: 'A4',
+                border: '1cm',
+                type: 'pdf'
+            };
+            pdf.create(html, options, function(err, buffer) {
+                file.path = file.base + file.relative.replace('.html', '.pdf');
+                file.contents = buffer;
+                cb(null, file);
+            });
+        });
+    },
 
-	renderHtml: function() {
-		return through.obj(function(file, enc, cb) {
-			var value = file.contents.toString();
-			value = toc.insert(value, {
-				maxDepth: 2
-			});
+    renderHtml: function() {
+        return through.obj(function(file, enc, cb) {
+            var value = file.contents.toString();
+            value = toc.insert(value, {
+                maxDepth: 2
+            });
 
-			file.contents = new Buffer(value);
+            file.contents = new Buffer(value);
 
-			var markedOptions = {
-				renderer: renderer
-			};
+            var markedOptions = {
+                renderer: renderer
+            };
 
-			marked(file.contents.toString(), markedOptions, function (err, data) {
-				if (err) {
-					cb(new Error('plugin error (markdown-render)', err, {fileName: file.path}));
-					return;
-				}
+            marked(file.contents.toString(), markedOptions, function (err, data) {
+                if (err) {
+                    return cb(new Error(
+                        'plugin error (markdown-render)',
+                        err, { fileName: file.path }));
+                }
 
-				file.contents = new Buffer(data);
-				file.path = replaceExtension(file.path, '.html');
-				var css = fs.readFileSync(require.resolve('./layout.css'), { encoding: 'utf8' });
-				var layout = fs.readFileSync(require.resolve('./layout.html'), { encoding: 'utf8' });
+                file.contents = new Buffer(data);
+                file.path = replaceExtension(file.path, '.html');
 
-				var html = layout
-					.replace('<!-- css -->', css)
-					.replace("<!-- contents -->", file.contents.toString());
+                var css = fs.readFileSync(
+                    require.resolve('./layout.css'), { encoding: 'utf8' });
 
-				file.contents = new Buffer(html);
+                var layout = fs.readFileSync(
+                    require.resolve('./layout.html'), { encoding: 'utf8' });
 
-				cb(null, file);
-			});
-		});
-	}
+                var html = layout
+                    .replace('<!-- css -->', css)
+                    .replace("<!-- contents -->", file.contents.toString());
+
+                file.contents = new Buffer(html);
+
+                cb(null, file);
+            });
+        });
+    }
 };
 
 
@@ -70,10 +75,10 @@ function replaceExtension(npath, ext){
 
 var renderer = new marked.Renderer();
 renderer.heading = function (text, level) {
-	var escapedText = text.toLowerCase().replace(/[ \/\?\*]+/g, '-');
-	return '<h' + level + '><a name="'
-		+ escapedText + '" class="anchor" href="#'
-		+ escapedText + '"><span class="header-link"></span></a>'
-		+ text + '</h' + level + '>';
+    var escapedText = text.toLowerCase().replace(/[ \/\?\*]+/g, '-');
+    return '<h' + level + '><a name="'
+        + escapedText + '" class="anchor" href="#'
+        + escapedText + '"><span class="header-link"></span></a>'
+        + text + '</h' + level + '>';
 };
 
